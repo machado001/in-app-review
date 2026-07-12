116d0e7d16241eeffb983568118352826c349047
Gabriel Machado <101916850+machado001@users.noreply.github.com>
2026-07-12 13:14:34 -0300

    upgrade agp (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -14,9 +13,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
         targetCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
-    }
-    kotlinOptions {
-        jvmTarget = libs.versions.javaVersion.get()
     }
 }
 
